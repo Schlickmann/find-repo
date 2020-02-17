aacf07783619b3fd6b257983d8c5f150c4223720
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-02-17 14:35:04 -0800

    retrieving repo info and issues

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
 export default function Repository({ match }) {
-  return <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>;
+  const [repo, setRepo] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadInfo() {
+      const repoName = decodeURIComponent(match.params.repository);
+
+      const [repoInfo, repoIssues] = await Promise.all([
+        api.get(`/repos/${repoName}`),
+        api.get(`/repos/${repoName}/issues`, {
+          params: {
+            state: 'open',
+            per_page: 5,
+          },
+        }),
+      ]);
+
+      setRepo(repoInfo.data);
+      setIssues(repoIssues.data);
+      setIsLoading(false);
+    }
+
+    loadInfo();
+  }, []);
+
+  return (
+    <>
+      <h1>Repo: {repo.name}</h1>
+    </>
+  );
 }
